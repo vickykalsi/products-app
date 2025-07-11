592cf6bf606373ed1044a0df32a587c81d1cde58
Harpuneet <hpkalsi16@gmail.com>
2025-07-11 22:18:22 +0530

    use render pg data

--- backend/config/db.js
+++ backend/config/db.js
@@ -3,11 +3,11 @@ import env from "dotenv";
 env.config();
 
 const client=new pg.Client({
-  user:"postgres",
-  database:"products",
-  password:`${process.env.DB_PASSWORD}`,
-  host:"localhost",
-  port:"5432"
+  user: process.env.PGUSER,
+  password: process.env.PGPASSWORD,
+  database: process.env.PGDATABASE,
+  host: process.env.PGHOST,
+  port: process.env.PGPORT
 });
 
 export default client;
