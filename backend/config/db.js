0cb2a8a3709e9b04188505eb93d728cfc79aa06a
Harpuneet <hpkalsi16@gmail.com>
2025-07-11 22:39:36 +0530

    use render pg string

--- backend/config/db.js
+++ backend/config/db.js
@@ -2,12 +2,11 @@ import pg from "pg";
 import env from "dotenv";
 env.config();
 
-const client=new pg.Client({
-  user: process.env.PGUSER,
-  password: process.env.PGPASSWORD,
-  database: process.env.PGDATABASE,
-  host: process.env.PGHOST,
-  port: process.env.PGPORT
+const client = new pg.Client({
+  connectionString: process.env.DATABASE_URL,
+  ssl: {
+    rejectUnauthorized: false
+  }
 });
 
 export default client;
