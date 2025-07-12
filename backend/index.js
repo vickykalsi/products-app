370d62f2fb0c50b0f0f0655f184e783964d5fadb
Harpuneet <hpkalsi16@gmail.com>
2025-07-12 15:28:14 +0530

    set helmet content security policy

--- backend/index.js
+++ backend/index.js
@@ -16,7 +16,7 @@ const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
 app.use(cors());
-app.use(helmet());
+app.use(helmet({contentSecurityPolicy:false}));
 app.use(morgan("dev"));
 app.use(express.json());
 app.use("/api/v1/products", productRouter);
