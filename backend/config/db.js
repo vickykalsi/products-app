99382180c838e033f70bbf4c6ea40f61aeaee31c
Harpuneet <hpkalsi16@gmail.com>
2025-07-12 13:51:24 +0530

    make use of node env variable

--- backend/config/db.js
+++ backend/config/db.js
@@ -2,11 +2,24 @@ import pg from "pg";
 import env from "dotenv";
 env.config();
 
-const client = new pg.Client({
-  connectionString: process.env.DATABASE_URL,
-  ssl: {
-    rejectUnauthorized: false
-  }
-});
+let client;
+if (process.env.NODE_ENV == "production") {
+  client = new pg.Client({
+    connectionString: process.env.DATABASE_URL,
+    ssl: {
+      rejectUnauthorized: false
+    }
+  });
+}
+else {
+  client = new pg.Client({
+    user: "postgres",
+    database: "products",
+    password: process.env.DB_PASSWORD,
+    host: "localhost",
+    port: 5432
+  });
+}
+
 
 export default client;
--- backend/index.js
+++ backend/index.js
@@ -15,11 +15,10 @@ const port = process.env.PORT;
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
-app.use(express.static(path.join(__dirname, "../frontend/dist")));
 app.use(cors());
-app.use(express.json());
 app.use(helmet());
 app.use(morgan("dev"));
+app.use(express.json());
 app.use("/api/v1/products", productRouter);
 
 const startDB = async function () {
@@ -27,10 +26,10 @@ const startDB = async function () {
     console.log("starting db");
     await client.connect();
     await client.query(`create table if not exists products
-    (id serial primary key,
-    name text not null,
-    image text not null,
-    price decimal(10,2) not null)`);
+      (id serial primary key,
+      name text not null,
+      image text not null,
+      price decimal(10,2) not null)`);
     console.log("db started");
   }
   catch (error) {
@@ -39,9 +38,12 @@ const startDB = async function () {
   }
 }
 
-app.get("*", (req, res) => {
-  res.sendFile(path.join(__dirname, "../frontend/dist/index.html"));
-});
+if (process.env.NODE_ENV == "production") {
+  app.use(express.static(path.join(__dirname, "../frontend/dist")));
+  app.get("*", (req, res) => {
+    res.sendFile(path.join(__dirname, "../frontend/dist/index.html"));
+  });
+}
 
 startDB().then(() => {
   app.listen(port, () => {
